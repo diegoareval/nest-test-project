ffbf8952ac8c2148fa35fb8c5e32d222df50242d
Diego <diego2000avelar@gmail.com>
2024-01-04 21:03:41 -0600

    feat: add documentation

--- nest-serializer-json/src/json-serializer/dtos/json.dto.ts
+++ nest-serializer-json/src/json-serializer/dtos/json.dto.ts
@@ -1,77 +1,95 @@
 import { IsString, IsObject, ValidateNested, IsArray, IsBoolean, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 class ActionDTO {
+  @ApiProperty()
   @IsString()
   type: string;
 
+  @ApiProperty()
   @IsString()
   topicArn: string;
 }
 
 class SpamVerdictDTO {
+  @ApiProperty()
   @IsString()
   status: string;
 }
 
 class VirusVerdictDTO {
+  @ApiProperty()
   @IsString()
   status: string;
 }
 
 class SpfVerdictDTO {
+  @ApiProperty()
   @IsString()
   status: string;
 }
 
 class DkimVerdictDTO {
+  @ApiProperty()
   @IsString()
   status: string;
 }
 
 class DmarcVerdictDTO {
+  @ApiProperty()
   @IsString()
   status: string;
 }
 
 class ReceiptDTO {
+  @ApiProperty()
   @IsString()
   timestamp: string;
 
+  @ApiProperty()
   @IsNumber()
   processingTimeMillis: number;
 
+  @ApiProperty()
   @IsArray()
   recipients: string[];
 
+  @ApiProperty({ type: SpamVerdictDTO })
   @IsObject()
   @ValidateNested()
   @Type(() => SpamVerdictDTO)
   spamVerdict: SpamVerdictDTO;
 
+  @ApiProperty({ type: VirusVerdictDTO })
   @IsObject()
   @ValidateNested()
   @Type(() => VirusVerdictDTO)
   virusVerdict: VirusVerdictDTO;
 
+  @ApiProperty({ type: SpfVerdictDTO })
   @IsObject()
   @ValidateNested()
   @Type(() => SpfVerdictDTO)
   spfVerdict: SpfVerdictDTO;
 
+  @ApiProperty({ type: DkimVerdictDTO })
   @IsObject()
   @ValidateNested()
   @Type(() => DkimVerdictDTO)
   dkimVerdict: DkimVerdictDTO;
 
+  @ApiProperty({ type: DmarcVerdictDTO })
   @IsObject()
   @ValidateNested()
   @Type(() => DmarcVerdictDTO)
   dmarcVerdict: DmarcVerdictDTO;
 
+  @ApiProperty()
   @IsString()
   dmarcPolicy: string;
 
+  @ApiProperty({ type: ActionDTO })
   @IsObject()
   @ValidateNested()
   @Type(() => ActionDTO)
@@ -79,52 +97,68 @@ class ReceiptDTO {
 }
 
 class HeadersDTO {
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsString()
   value: string;
 }
 
 class CommonHeadersDTO {
+  @ApiProperty()
   @IsString()
   returnPath: string;
 
+  @ApiProperty()
   @IsArray()
   from: string[];
 
+  @ApiProperty()
   @IsString()
   date: string;
 
+  @ApiProperty()
   @IsArray()
   to: string[];
 
+  @ApiProperty()
   @IsString()
   messageId: string;
 
+  @ApiProperty()
   @IsString()
   subject: string;
 }
 
 class MailDTO {
+  @ApiProperty()
   @IsString()
   timestamp: string;
 
+  @ApiProperty()
   @IsString()
   source: string;
 
+  @ApiProperty()
   @IsString()
   messageId: string;
 
+  @ApiProperty()
   @IsArray()
   destination: string[];
 
+  @ApiProperty()
   @IsBoolean()
   headersTruncated: boolean;
 
+  @ApiProperty()
   @IsArray()
+  @Type(() => HeadersDTO)
   headers: HeadersDTO[];
 
+  @ApiProperty({ type: CommonHeadersDTO })
   @IsObject()
   @ValidateNested()
   @Type(() => CommonHeadersDTO)
@@ -132,38 +166,38 @@ class MailDTO {
 }
 
 class SesDtoSlice {
-    @IsObject()
-    @ValidateNested()
-    @Type(() => ReceiptDTO)
-    receipt: ReceiptDTO;
-  
-    @IsObject()
-    @ValidateNested()
-    @Type(() => MailDTO)
-    mail: MailDTO;
+  @ApiProperty({ type: ReceiptDTO })
+  @IsObject()
+  @ValidateNested()
+  @Type(() => ReceiptDTO)
+  receipt: ReceiptDTO;
+
+  @ApiProperty({ type: MailDTO })
+  @IsObject()
+  @ValidateNested()
+  @Type(() => MailDTO)
+  mail: MailDTO;
 }
 
 class SesDTO {
+  @ApiProperty()
   @IsString()
   eventVersion: string;
 
+  @ApiProperty({ type: SesDtoSlice })
   @IsObject()
   @ValidateNested()
   @Type(() => SesDtoSlice)
   ses: SesDtoSlice;
 
+  @ApiProperty()
   @IsString()
   eventSource: string;
-
 }
 
 export class JsonSerializerDTO {
+  @ApiProperty({ type: [SesDTO] })
   @IsArray()
-  @IsObject({ each: true })
-  @ValidateNested({ each: true })
   @Type(() => SesDTO)
   Records: SesDTO[];
 }
-
-
-
--- nest-serializer-json/src/json-serializer/json-serializer.controller.ts
+++ nest-serializer-json/src/json-serializer/json-serializer.controller.ts
@@ -1,12 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import {  ProcessedJsonDTO, JsonSerializerDTO } from './dtos';
+import { ProcessedJsonDTO, JsonSerializerDTO } from './dtos';
 import { JsonSerializerService } from './json-serializer.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('JsonParser')
 @Controller('email')
 export class JsonSerializerController {
   constructor(private readonly jsonSerializerService: JsonSerializerService) {}
 
   @Post('process')
+  @ApiResponse({
+    status: 201,
+    description: 'Data parsed',
+    type: ProcessedJsonDTO,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   processEmail(@Body() email: JsonSerializerDTO): ProcessedJsonDTO {
     return this.jsonSerializerService.processEmails(email);
   }
